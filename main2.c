0b2477541d958b74621cd1c2db2f64c92a75fb4d
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-12-12 06:33:15 +0000

    all

---
+++ main2.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <readline/readline.h>
+//#include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void    execute_command(char *command)
+{
+    pid_t pid = fork();
+
+    if(pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid == 0)
+    {
+        if
+    }
+}
