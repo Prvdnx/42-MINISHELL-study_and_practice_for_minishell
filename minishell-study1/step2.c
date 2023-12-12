a567e01fcee6f4765500ec41d7ffd70f6db82fb6
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-12-12 10:09:03 +0000

    commit

--- minishell-study1/step2.c
+++ minishell-study1/step2.c
@@ -22,7 +22,7 @@ int main(void)
     free(input);
     return (0);
 }
-// compile with: cc step1.c -lreadline -o minishell
+// compile with: cc step2.c -lreadline -o minishell
 
 /* EXPLANATION
 
---
+++ minishell-study1/step4.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void    execute_command(char *command, char **args)
+{
+    pid_t pid = fork();
+
+    if(pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid == 0)
+    {
+        if(execve(command, args, NULL) == -1)
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(NULL);
+    }
+}
