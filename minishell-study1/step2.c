be0da351ae7f1bb441ff753f9a129fcfd0235ece
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-12-12 10:00:26 +0000

    commit

--- minishell-study1/step2.c
+++ minishell-study1/step2.c
@@ -4,74 +4,48 @@
 #include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <sys/wait.h>
-#include <string.h>
 
-void    execute_command(char *command)
+// Step 1 & 2
+int main(void)
 {
-    pid_t pid = fork();
-
-    if(pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if(pid == 0)
-    {
-        if(execve(command, NULL, NULL) == -1)
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        wait(NULL);
-    }
-}
-
-int main (void)
-{
+    printf("Welcome to my Minishell!\n");
     char *input = readline("minishell> ");
 
-    if(input == NULL)
-    {
-        printf("Exiting minishell\n");
-        exit(EXIT_SUCCESS);
-    }
-
+    if (input == NULL)
+        {
+            printf("Exiting minshell\n");
+            exit(EXIT_SUCCESS);
+        }
     add_history(input);
-
     printf("You entered: %s\n", input);
 
-    char *command = strtok(input, " ");
-
-    if(strcmp(command, "exit") == 0)
-    {
-        printf("Exiting minishell\n");
-        exit(EXIT_SUCCESS);
-    }
-
-    execute_command(command);
-
     free(input);
-
-    return(0);
+    return (0);
 }
+// compile with: cc step1.c -lreadline -o minishell
 
-/*Explanation:
+/* EXPLANATION
 
-execute_command Function:
-I created a new function called executeCommand to encapsulate the logic related to forking and executing the command.
-The function takes a char* parameter (command), representing the command to be executed.
+#include <stdio.h>: Standard Input/Output library for functions like printf.
 
-Forking and Executing in execute_command:
-Inside execute_command, I kept the code responsible for forking and executing the command.
-It forks a new process, and in the child process, it executes the command using execve.
+#include <stdlib.h>: Standard library for functions like exit and dynamic memory allocation (malloc, free).
 
-Main Function:
-The main function now focuses on user input and high-level control flow.
-It reads user input using readline, adds it to the history, and displays the entered command.
-The tokenization logic is kept here for now (to be implemented in later steps). It splits the input
-into tokens, and we assume a simple command without arguments for now.
-It checks if the command is "exit" and exits the shell if true.
-The actual execution of the command is delegated to the executeCommand function.*/
+#include <unistd.h>: Unix Standard library for functions like fork, execve, pipe, etc.
+
+#include <readline/readline.h>: Library for readline function to get user input.
+
+#include <readline/history.h>: Library for history related functions (add_history).
+
+int main(void) {: Main function declaration.
+
+char *input = readline("minishell> ");: Display prompt "minishell> " and read user input using readline.
+
+if (input == NULL) { ... }: Check if readline encountered an error or received EOF (ctrl-D), and exit gracefully.
+
+add_history(input);: Add the input to the command history.
+
+printf("You entered: %s\n", input);: Display the entered command.
+
+free(input);: Free the memory allocated by readline.
+
+return 0;: Exit the program. */
---
+++ minishell-study1/step3.c
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void    execute_command(char *command)
+{
+    pid_t pid = fork();
+
+    if(pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid == 0)
+    {
+        if(execve(command, NULL, NULL) == -1)
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        wait(NULL);
+    }
+}
+
+int main (void)
+{
+    char *input = readline("minishell> ");
+
+    if(input == NULL)
+    {
+        printf("Exiting minishell\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    add_history(input);
+
+    printf("You entered: %s\n", input);
+
+    char *command = strtok(input, " ");
+
+    if(strcmp(command, "exit") == 0)
+    {
+        printf("Exiting minishell\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    execute_command(command);
+
+    free(input);
+
+    return(0);
+}
+
+/*Explanation:
+
+execute_command Function:
+I created a new function called executeCommand to encapsulate the logic related to forking and executing the command.
+The function takes a char* parameter (command), representing the command to be executed.
+
+Forking and Executing in execute_command:
+Inside execute_command, I kept the code responsible for forking and executing the command.
+It forks a new process, and in the child process, it executes the command using execve.
+
+Main Function:
+The main function now focuses on user input and high-level control flow.
+It reads user input using readline, adds it to the history, and displays the entered command.
+The tokenization logic is kept here for now (to be implemented in later steps). It splits the input
+into tokens, and we assume a simple command without arguments for now.
+It checks if the command is "exit" and exits the shell if true.
+The actual execution of the command is delegated to the executeCommand function.*/
