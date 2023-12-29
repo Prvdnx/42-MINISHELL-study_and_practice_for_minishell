7d3874badc3dc03ce7381e6a46ec66e68a86279b
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-12-29 17:04:13 +0000

    o

--- Basic_shell_study/6_EXECVE.c
+++ Basic_shell_study/6_EXECVE.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 ////// 1 //////
-/*int main()
+int main()
 {
     char *argv[] = {"/bin/ls", "-l", NULL};
 
@@ -16,37 +16,37 @@
     printf("Done with execve!");
 
     return (0);
-}*/
+}
 
 
 
 ////// 2 //////
-#include <sys/types.h>
-#include <sys/wait.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
 
-int main()
-{
-    pid_t pid;
+// int main()
+// {
+//     pid_t pid;
 
-    char *argv[] = {"/bin/ls", "-l", NULL};
+//     char *argv[] = {"/bin/ls", "-l", NULL};
 
-    pid = fork();
+//     pid = fork();
 
-    if (pid == -1)
-        return (-1);
+//     if (pid == -1)
+//         return (-1);
 
-    if (pid == 0)
-    {
-        int value = execve(argv[0], argv, NULL);
+//     if (pid == 0)
+//     {
+//         int value = execve(argv[0], argv, NULL);
 
-        if(value == -1)
-            perror("Error");
-    }
-    else
-    {
-        wait(NULL);
-        printf("Done with execve!");
-    }
-    return (0);
-}
+//         if(value == -1)
+//             perror("Error");
+//     }
+//     else
+//     {
+//         wait(NULL);
+//         printf("Done with execve!");
+//     }
+//     return (0);
+// }
 
