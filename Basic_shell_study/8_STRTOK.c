a2c7605d34e730754e0f91e5a386e3bcb0915a2c
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2023-12-29 17:54:54 +0000

    o

--- Basic_shell_study/8_STRTOK.c
+++ Basic_shell_study/8_STRTOK.c
@@ -5,16 +5,73 @@
 #include <string.h>
 
 ////////// 1 //////////
+// int main(void)
+// {
+//     char str[] = "Jesus Loves You";
+//     char *delim = " ";
+//     char *token;
+
+//     token = strtok(str, delim);
+//     printf("%s\n", token);
+
+//     return (0);
+// }
+
+
+////////// 2 //////////
+// int main(void)
+// {
+//     char str[] = "Jesus Loves You";
+//     char *delim = " ";
+//     char *token;
+
+//     token = strtok(str, delim);
+//     printf("%s ", token);
+
+//     token = strtok(NULL, delim);
+//     printf("%s\n", token);
+
+//     return (0);
+// }
+
+
+////////// 3 //////////
+// int main(void)
+// {
+//     char str[] = "Jesus Loves You";
+//     //char *str = "Jesus Loves You"; --> Using this would cause a "Segmentation fault (core dumped)"
+//     char *delim = " ";
+//     char *token;
+
+//     token = strtok(str, delim);
+
+//     while (token != NULL)
+//     {
+//         printf("%s\n", token);
+//         token = strtok(NULL, delim);
+//     }
+    
+//     return (0);
+// }
+
+
+////////// 4 //////////
 int main(void)
 {
-    char str[] = "Jesus Loves You";
+    char *src = "Jesus Loves You";
+    char *str = malloc(sizeof(char) * strlen(src));
     char *delim = " ";
     char *token;
 
+    strcpy(str, src);
+
     token = strtok(str, delim);
 
-    printf("%s", token);
+    while (token != NULL)
+    {
+        printf("%s\n", token);
+        token = strtok(NULL, delim);
+    }
+    
     return (0);
-
 }
-
